b0502c9e16ff7e59b8b542f3f045f27475bb691d
IlliaTkachuk <62558542+IlliaTkachuk@users.noreply.github.com>
2020-03-26 01:22:22 +0200

    1st draft

--- src/main/java/CountManager/Main.java
+++ src/main/java/CountManager/Main.java
@@ -17,78 +17,79 @@ import java.io.InputStreamReader;
    или вручную (навигация по календарю с возможностью ручной замены параметров)
  */
 
-/*
-   Написать класс со статическими методами визуального оформления (может даже псевдографика)
- */
+//   Написать класс со статическими методами визуального оформления (может даже псевдографика)
 
-
+/**
+ * Assumes AuthService.login and AuthService.setUp are implemented correctly
+*/
 public class Main {
+
+    private static final String WELCOME_MSG = "***Welcome to CountingManager v2.0!*** \nPlease type according digits for navigation";
+    private static final String AUTH_PROMPT_MSG = "Do you already have an account? \n1.Yes, login me in   2.No, sign me up   0.Exit";
+    private static final String MAIN_MENU_MSG = "*** Main Menu *** \n1.Add New Work \n2.See Recorded Work \n3.Payment Details Setup\n" +
+            "4.Calculate Revenue \n0.Log out";
+    private static final String WRONG_INPUT_MSG = "Incorrect input. Please, use digits to navigate";
+    private static final String WRONG_OPTION_MSG = "Invalid Option. Please, try again";
+    private static boolean endOfProgram = false;
 
     private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    // Нужно сделать статическое поле answer, но я пока не могу
-    // придумать как сравнивать его с 0 на протяжении программы (кроме тупой проверки)
+    // Нужно сделать статическое поле answer (или же использовать @endOfProgram для этого),
+    // но я пока не могу придумать как сравнивать его с 0 на протяжении программы (кроме тупой проверки)
     // +нужно додуматься как его использовать в while
 
     public static void main(String[] args) {
         try {
-//            AuthService.login();        // еще не разобрался почему signUp и login имеют именно такие сигнатуры
+//          AuthService.login(new User());        // еще не разобрался почему signUp и login имеют именно такие сигнатуры
+            System.out.println(WELCOME_MSG);
             greetingScreen();
-            mainMenu();
+            if (!endOfProgram) mainMenu();
 
-        } catch (Exception e) {
-                System.out.println("Incorrect input. Please, use digits to navigate");
-        }
-/*
-        finally {
-            try {
-                Main.reader.close();
-            } catch (IOException e) {}
-        }
-*/
-
+        } catch (Exception e) { System.out.println(WRONG_INPUT_MSG); }
     }
 
     private static void greetingScreen() throws IOException {
 
-        System.out.println("***Welcome to CountingManager v2.0!***");
-        System.out.println("Please type according digits for navigation");
-        System.out.println();
-        System.out.println("Do you already have an account?");
-        System.out.println("1.Yes, login me in   2.No, sign me up   0.Exit");
+        System.out.println(AUTH_PROMPT_MSG);
         int answer = Integer.parseInt(reader.readLine());
-        System.out.println();
 
-           /*
-                switch (answer) {
+        switch (answer) {
+             case 0: {
+                 exitMain();
+                 break;
+             }
              case 1: {
-                 AuthService.login(User user);
+//               AuthService.login(new User()));
+                 System.out.println("Log in successful!");
+                 System.out.println();
                  break;
              }
              case 2: {
-                 AuthService.signUp(User user)
+//               AuthService.signUp(User user)
+                 System.out.println("Sign up successful! Now log in using these credentials");
+                 System.out.println();
+                 greetingScreen();
+//               AuthService.login(new User());
                  break;
              }
-             case 0: exitMain();            // Останавливает приложение полностью (закрыть ридер)
              default: {
-                 System.out.println("Incorrect Input. Please, try again");
+                 System.out.println(WRONG_OPTION_MSG);
+                 greetingScreen();
              }
         }
-        */
-
     }
 
     private static void mainMenu() throws IOException {
-        System.out.println("*** Main Menu ***");
-        System.out.println("1.Add New Work");
-        System.out.println("2.See Recorded Work");
-        System.out.println("3.Payment Details Setup");
-        System.out.println("4.Calculate Revenue");
-        System.out.println("0.Previous menu");
+        System.out.println(MAIN_MENU_MSG);
 
         switch (Integer.parseInt(reader.readLine())) {
+
+            case 0: {
+                greetingScreen();
+                break;
+            }
             case 1: {
-                // Calendarная хрень
+                // Calendarная хрень @workMenu()
                 // Научиться хранить объекты (e.g. записывать их файлы и как удобнее это организовать)
                 break;
             }
@@ -108,13 +109,24 @@ public class Main {
                 // Использует countIncome(); сорри за херовое название класса & метода
                 break;
             }
-//            case 0: greetingScreen();
             default: {
-                System.out.println("Invalid Input.");
+                System.out.println(WRONG_OPTION_MSG);
                 mainMenu();
             }
         }
     }
+
+    //TODO: implement
     private static void workMenu(){}
 
+
+//    Дает флаг о закрытии программы
+    private static void exitMain(){
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        endOfProgram = true;
+    }
 }
