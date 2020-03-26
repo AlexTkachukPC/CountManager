f6e36a16681452f8f7e21a5cd4d807bb96cd7411
AlexTkachukPC <AlexTkachukPC@gmail.com>
2020-03-26 23:14:22 +0200

    refactor Main added Constants and packages

--- src/main/java/CountManager/Main.java
+++ src/main/java/CountManager/Main.java
@@ -1,15 +1,13 @@
 package CountManager;
 
-import CountManager.domain.User;
-import CountManager.service.AuthService;
 import CountManager.service.ConsoleService;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import static CountManager.constants.MessageConstants.*;
 import static CountManager.service.ConsoleService.readInteger;
+import static CountManager.service.ConsoleService.writeMessage;
+import static CountManager.service.MessageService.*;
 
 /* В будущем можно добавить возможность переключения между модами:
    1. Видеть данные за последнюю неделю/месяц
@@ -29,95 +27,16 @@ import static CountManager.service.ConsoleService.readInteger;
 public class Main {
     private static boolean endOfProgram = false;
 
-
     // Нужно сделать статическое поле answer (или же использовать @endOfProgram для этого),
     // но я пока не могу придумать как сравнивать его с 0 на протяжении программы (кроме тупой проверки)
     // +нужно додуматься как его использовать в while
 
     public static void main(String[] args) {
-        try {
-//          AuthService.login(new User());        // еще не разобрался почему signUp и login имеют именно такие сигнатуры
-            System.out.println(WELCOME_MSG);
-            greetingScreen();
-            if (!endOfProgram) mainMenu();
+        // вот тут смотри, это бесконечный цикл тут просто когда будет выбран exit, просто пишем прощание и  делаем break и все программа закончилась.
+        // Если exit когда пользователь выходит из аккаунта, то просто выходим опять в меню signUp и LogIn
+        while (true) {
 
-        } catch (Exception e) { System.out.println(WRONG_INPUT_MSG); }
-    }
 
-    private static void greetingScreen() throws IOException {
-
-        System.out.println(AUTH_PROMPT_MSG);
-        int answer = readInteger();
-
-        switch (answer) {
-             case 0: {
-                 exitMain();
-                 break;
-             }
-             case 1: {
-//               AuthService.login(new User()));
-                 System.out.println("Log in successful!");
-                 System.out.println();
-                 break;
-             }
-             case 2: {
-//               AuthService.signUp(User user)
-                 System.out.println("Sign up successful! Now log in using these credentials");
-                 System.out.println();
-                 greetingScreen();
-//               AuthService.login(new User());
-                 break;
-             }
-             default: {
-                 System.out.println(WRONG_OPTION_MSG);
-                 greetingScreen();
-             }
         }
-    }
-
-    private static void mainMenu() throws IOException {
-        System.out.println(MAIN_MENU_MSG);
-
-        switch (readInteger()) {
-
-            case 0: {
-                greetingScreen();
-                break;
-            }
-            case 1: {
-                // Calendarная хрень @workMenu()
-                // Научиться хранить объекты (e.g. записывать их файлы и как удобнее это организовать)
-                break;
-            }
-            case 2: {
-                // Пользуясь коллекциями вывести данные из хранилища программы
-                // workMenu();
-                break;
-            }
-            case 3: {
-                // Позволяет ввести расчётные константы (з/п, налог, доплата)
-                // Константы должны быть общие для проекта (будут использованы в calc.IncomeCount)
-                // Применить для: выходных/будних
-                break;
-            }
-            case 4: {
-                // Предлагает провести расчет для недели/месяца и тд.
-                // Использует countIncome(); сорри за херовое название класса & метода
-                break;
-            }
-            default: {
-                System.out.println(WRONG_OPTION_MSG);
-                mainMenu();
-            }
-        }
-    }
-
-    //TODO: implement
-    private static void workMenu(){}
-
-
-//    Дает флаг о закрытии программы
-    private static void exitMain(){
-        endOfProgram = true;
     }
 }
--- src/main/java/CountManager/service/MessageService.java
+++ src/main/java/CountManager/service/MessageService.java
@@ -1,4 +1,108 @@
 package CountManager.service;
 
+import CountManager.constants.MessageConstants;
+
+import java.io.IOException;
+
+import static CountManager.constants.MessageConstants.*;
+import static CountManager.service.ConsoleService.readInteger;
+import static CountManager.service.ConsoleService.writeMessage;
+
 public class MessageService {
+
+    public static void printMainMenuMessage() {
+        writeMessage(MAIN_MENU_MSG);
+        writeMessage("\n");
+    }
+
+
+    public static void printWelcomeMessage() {
+        writeMessage(WELCOME_MSG);
+        writeMessage("\n");
+    }
+
+    public static void printAuthPromptMessage() {
+        writeMessage(AUTH_PROMPT_MSG);
+        writeMessage("\n");
+    }
+
+    public static void printWrongInputMessage() {
+        writeMessage(WRONG_INPUT_MSG);
+        writeMessage("\n");
+    }
+
+    public static void printWrongOptionMessage() {
+        writeMessage(WRONG_OPTION_MSG);
+        writeMessage("\n");
+    }
+
+    private static void greetingScreen() throws IOException {
+
+        printAuthPromptMessage();
+        int answer = readInteger();
+
+        switch (answer) {
+            case 0: {
+                break;
+            }
+            case 1: {
+//               AuthService.login(new User()));
+                writeMessage("Log in successful!");
+                writeMessage("\n");
+                break;
+            }
+            case 2: {
+//               AuthService.signUp(User user)
+                writeMessage("Sign up successful! Now log in using these credentials");
+                writeMessage("\n");
+                greetingScreen();
+//               AuthService.login(new User());
+                break;
+            }
+            default: {
+                printWrongOptionMessage();
+                greetingScreen();
+            }
+        }
+    }
+
+    private static void mainMenu() throws IOException {
+        writeMessage(MAIN_MENU_MSG);
+
+        switch (readInteger()) {
+
+            case 0: {
+                greetingScreen();
+                break;
+            }
+            case 1: {
+                // Calendarная хрень @workMenu()
+                // Научиться хранить объекты (e.g. записывать их файлы и как удобнее это организовать)
+                break;
+            }
+            case 2: {
+                // Пользуясь коллекциями вывести данные из хранилища программы
+                // workMenu();
+                break;
+            }
+            case 3: {
+                // Позволяет ввести расчётные константы (з/п, налог, доплата)
+                // Константы должны быть общие для проекта (будут использованы в calc.IncomeCount)
+                // Применить для: выходных/будних
+                break;
+            }
+            case 4: {
+                // Предлагает провести расчет для недели/месяца и тд.
+                // Использует countIncome(); сорри за херовое название класса & метода
+                break;
+            }
+            default: {
+                printWrongOptionMessage();
+                mainMenu();
+            }
+        }
+    }
+
+    //TODO: implement
+    private static void workMenu(){}
 }
